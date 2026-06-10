318eddf6e0dc32be1f41421ba2a763158ea455cc
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-06-10 19:46:04 +0300

    fix: pycodestyle

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range (10):
-    for j in range (10):
+for i in range(10):
+    for j in range(10):
         if i >= j:
             continue
         elif i == 8 and j == 9:
