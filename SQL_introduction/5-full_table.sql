1ac1f20fadcb3043d3e094f749bdb09d45fba56a
muhammaduop1427 <muhammaduop1427@gmail.com>
2026-08-05 17:31:59 +0300

    task5 edit1

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
--- Write a script that prints the description of the table first_table from the database hbtn_0c_0 without using DESCRIBE or EXPLAIN --
-SELECT COLUMN_NAME AS Field, COLUMN_TYPE AS Type, IS_NULLABLE AS Null, COLUMN_KEY AS Key, COLUMN_DEFAULT AS Default, EXTRA AS Extra 
-FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+-- Write a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server. --
+SHOW CREATE TABLE first_table;
